58be25b11959f9df14d4364d42ff12f7e4f1b6b8
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-23 14:45:31 +0100

    Add separate config for preview.

--- config.py
+++ config.py
@@ -5,8 +5,15 @@ class Config(object):
     DEBUG = False
 
 
-class DevelopmentConfig(object):
+class DevelopmentConfig(Config):
     DEBUG = True
+    DATABASE_NAME = 'db2'
+    DATABASE_USER = 'landcharges'
+    DATABASE_PASSWORD = 'lcalpha'
+    DATABASE_HOST = 'localhost'
+
+
+class PreviewConfig(Config):
     DATABASE_NAME = 'db2'
     DATABASE_USER = 'landcharges'
     DATABASE_PASSWORD = 'lcalpha'
