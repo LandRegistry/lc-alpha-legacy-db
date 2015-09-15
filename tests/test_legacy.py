49d90ed7ce8350e7dbf17b25e5be2e46feceeba3
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-09-15 12:24:19 +0100

    Code, unit test & fix PEP8 violations while I'm here...

--- tests/test_legacy.py
+++ tests/test_legacy.py
@@ -4,41 +4,67 @@ import os
 import json
 
 
-class MockConnection:
-    def __init__(self, results):
-        self.results = results
-
-    def cursor(self):
-        return MockCursor(self.results, self)
-
-    def commit(self):
-        pass
-
-    def close(self):
-        pass
-
-
-class MockCursor:
-    def __init__(self, results, conn):
-        self.results = results
-        self.connection = conn
-
-    def execute(self, sql, dict):
-        pass
-
-    def close(self):
-        pass
-
-    def fetchall(self):
-        return self.results
-
-    def fetchone(self):
-        return [42]
+# class MockConnection:
+#     def __init__(self, results):
+#         self.results = results
+#
+#     def cursor(self):
+#         return MockCursor(self.results, self)
+#
+#     def commit(self):
+#         pass
+#
+#     def close(self):
+#         pass
+#
+#
+# class MockCursor:
+#     def __init__(self, results, conn):
+#         self.results = results
+#         self.connection = conn
+#
+#     def execute(self, sql, dict):
+#         pass
+#
+#     def close(self):
+#         pass
+#
+#     def fetchall(self):
+#         return self.results
+#
+#     def fetchone(self):
+#         return [42]
 
 
 dir = os.path.dirname(__file__)
 valid_data = open(os.path.join(dir, 'data/valid_data.json'), 'r').read()
-mock_connection = MockConnection(valid_data)
+# mock_connection = MockConnection(valid_data)
+
+keyholder_data = [{
+    'account_code': 'C',
+    'postcode': 'JJ75 3SC',
+    'name_length_1': 25,
+    'name_length_2': 0,
+    'address_length_1': 17,
+    'address_length_2': 17,
+    'address_length_3': 14,
+    'address_length_4': 0,
+    'address_length_5': 0,
+    'name': 'LESSIE LITTLE ASSOCIATES ;',
+    'address': '122 MAYERT TRAIL NORTH COLLINVIEW HEREFORDSHIRE ;'
+}]
+
+keyholder_found = {
+    'return_value': mock.Mock(**{
+        'cursor.return_value': mock.Mock(**{'fetchall.return_value': keyholder_data})
+    })
+}
+
+keyholder_not_found = {
+    'return_value': mock.Mock(**{
+        'cursor.return_value': mock.Mock(**{'fetchall.return_value': []})
+    })
+}
 
 
 class TestWorking:
@@ -49,13 +75,13 @@ class TestWorking:
         response = self.app.get("/")
         assert response.status_code == 200
 
-    @mock.patch('psycopg2.connect', return_value=mock_connection)
+    @mock.patch('psycopg2.connect')
     def test_add_to_db2(self, mock_connect):
         headers = {'Content-Type': 'application/json'}
         response = self.app.put('/land_charge', data=valid_data, headers=headers)
         assert response.status_code == 200
 
-    @mock.patch('psycopg2.connect', return_value=mock_connection)
+    @mock.patch('psycopg2.connect')
     def test_add_to_db2_invalid_data(self, mock_connect):
         headers = {'Content-Type': 'application/xml'}
         response = self.app.put('/land_charge', data=valid_data, headers=headers)
@@ -84,4 +110,16 @@ class TestWorking:
         response = self.app.put('/land_charge', data=valid_data, headers=headers)
         assert response.status_code == 500
 
+    @mock.patch('psycopg2.connect', **keyholder_found)
+    def test_get_keyholder(self, mock_connect):
+        response = self.app.get('/keyholder/2365870')
+        data = json.loads(response.data.decode('utf-8'))
+        assert response.status_code == 200
+        assert len(data['address']['address_lines']) == 3
+        assert len(data['name']) == 1
+        assert data['number'] == '2365870'
 
+    @mock.patch('psycopg2.connect', **keyholder_not_found)
+    def test_get_keyholder_not_found(self, mock_connect):
+        response = self.app.get('/keyholder/66666')
+        assert response.status_code == 404
