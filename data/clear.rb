db1d1c5c8f7004b96b26eb4dc074850e8e839318
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-11-10 13:37:09 +0000

    Convert data setup to ruby for acceptance test integration improvement

---
+++ data/clear.rb
@@ -0,0 +1,10 @@
+require 'net/http'
+require 'json'
+
+
+uri = URI(ENV['LEGACY_DB_URI'] || 'http://localhost:5007')
+http = Net::HTTP.new(uri.host, uri.port)
+response = http.request(Net::HTTP::Delete.new('/keyholders'))
+response = http.request(Net::HTTP::Delete.new('/debtors'))
+response = http.request(Net::HTTP::Delete.new('/complex_names'))
+response = http.request(Net::HTTP::Delete.new('/land_charges'))
--- data/delete.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-
-url = os.getenv('LEGACY_ADAPTER_URI', 'http://localhost:5007')
-response = requests.delete(url + '/debtors')
-response = requests.delete(url + '/keyholders')
-response = requests.delete(url + '/complex_names')
-response = requests.delete(url + '/land_charges')
