41dc17570f139dc389c86338ba26f0b7faca2c05
Rob Stephenson <robstephenson@digital.landregistry.gov.uk>
2016-01-20 11:26:52 +0000

    complex name searching of number reinstated

--- application/names.py
+++ application/names.py
@@ -7,10 +7,20 @@ def get_name_variants(connection, name):
                    {'name': "%{}%".format(name.upper())})
     rows = cursor.fetchall()
 
+    numbers = []
+    for row in rows:
+        numbers.append(row['number'])
+
+    cursor.execute('select number, name from name_variants where number=ANY(%(numbers)s)',
+                   {'numbers': numbers})
+
+    rows = cursor.fetchall()
+
     result = []
     for row in rows:
         result.append({
             'name': row['name'],
             'number': row['number']
         })
+    print(result)
     return result
